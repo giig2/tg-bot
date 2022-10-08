65021895fa19a3aa401d71d54d92bbe7e93f1df4
giig2 <105631331+giig2@users.noreply.github.com>
2022-10-08 15:56:48 +0500

    Create README.md

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from telebot import TeleBot, types
+import os
+import configparser
+config = configparser.ConfigParser()
+config.read("settings.ini")
+#
+#
+#
+TOKEN = config['DATA']['API_TOKEN']
+URL = config['DATA']['URL_HER']
+#
+#
+#
+app = Flask(__name__)
+bot = TeleBot(TOKEN)
+
+@app.route("/")
+def header():
+    bot.remove_webhook()
+    bot.set_webhook(URL)
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))

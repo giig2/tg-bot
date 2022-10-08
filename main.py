207ef54ac477d98f74e11cafd19bd6f6c8d65600
я <Shapkins9812@gmail.com>
2022-10-08 19:57:24 +0500

    Create README.md

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
 import os
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
 import configparser
+import logging
 config = configparser.ConfigParser()
 config.read("settings.ini")
 #
@@ -9,30 +13,28 @@ config.read("settings.ini")
 TOKEN = config['DATA']['API_TOKEN']
 URL = config['DATA']['URL_HER']
 URl_TOKEN = config['DATA']['URL_TOKEN']
-print(TOKEN)
+
 #
 #https://api.telegram.org/bot5711773496:AAHyXFloUrGAkN_XAIhSWpfR1_4NZ2l73lk/setWebhook?url=https://tgbot5544.herokuapp.com/
 #
 app = Flask(__name__)
-bot = TeleBot(token=TOKEN)
+bot = Bot(TOKEN)
+dp= Dispatcher(Bot(TOKEN))
+async def on_startup(dp):
+    await bot.set_webhook(URl_TOKEN, drop_pending_updates=True)
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
-@app.route("/")
-def header():
-    bot.remove_webhook()
-    bot.set_webhook(URl_TOKEN)
-    return '!', 200
-@app.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return '!', 200
-@bot.message_handler(commands=["start"])
-def start(message):
-    print(1)
-    bot.send_message(message.chat.id, "Привет")
+@dp.message_handler(commands=['start'])
+async def nnn(message):
+    await bot.send_message(message.chat.id, "Привет")
 
 
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=URl_TOKEN,
+        kip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown)

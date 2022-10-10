fb960be602be6fca0f53be92fb045be9aafd7394
я <Shapkins9812@gmail.com>
2022-10-10 22:46:49 +0500

    Create README.md

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ async def on_shutdown(dp):
 #
 
 @dp.message_handler(commands=['start'])
-async def startcom(message: types.Message):
-    await message.answer(f"Привет,{message.from_user.first_name}")
+async def startcom(msg: types.Message, message):
+    await msg.answer(f"Привет,{message.from_user.first_name}")
 
 
 if __name__ == '__main__':

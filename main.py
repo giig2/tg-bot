ca8e1f148a135b4365a8afa8514384aa2bb87ea5
я <Shapkins9812@gmail.com>
2022-10-14 19:26:35 +0500

    Create README.md

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Flask
+from flask import Flask, request
 import configparser
 from telebot import TeleBot, types
 config = configparser.ConfigParser()
@@ -26,49 +26,22 @@ bot = TeleBot(token=TOKEN)
 def start(message):
     bot.send_message(message.chat.id, "Привет")
 
-
-
-if __name__ == '__main__':
-    requests.get(WEBHOOK_SET)
-    bot.run_webhooks(listen="0.0.0.0",
-                     port=os.getenv('PORT'),
-                     webhook_url=URL,)
-
-
-import os
-import telebot
-from flask import Flask, request
-
-TOKEN = '1810242195:AAFfImqq9mPT81rs3ApL74eUob5u8A9oxb8'
-APP_URL = f'https://vitaljaheroku.herokuapp.com/{TOKEN}'
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo(message):
-    bot.reply_to(message, message.text)
-
-
-@server.route('/' + TOKEN, methods=['POST'])
+@app.route('/' + TOKEN, methods=['POST'])
 def get_message():
     json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
+    update = types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return '!', 200
 
-
-@server.route('/')
+@app.route('/')
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
+    bot.set_webhook(url=URl_TOKEN)
     return '!', 200
 
 
+
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    #requests.get(WEBHOOK_SET)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', default=5000)))
+

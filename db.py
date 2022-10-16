90a6dc462557c7d4ecfa46065e0b55bf8973aef6
я <Shapkins9812@gmail.com>
2022-10-16 11:52:17 +0500

    Create README.md

--- db.py
+++ db.py
@@ -7,21 +7,20 @@ config = configparser.ConfigParser()
 config.read("settings.ini")
 TOKEN = config['DATA']['API_TOKEN']
 bot = Bot(token=TOKEN)
+connect = sqlite3.connect('name.db', check_same_thread=False)
+commit = connect.commit()
 #dp= Dispatcher(Bot(TOKEN))
 
 class load(StatesGroup):
     name = State()
 
-class DATABASE():
-    connect = sqlite3.connect('name.db', check_same_thread=False)
-    commit = connect.commit()
-    #def create_table(self, chat_id):
+ #def create_table(self, chat_id):
         #self.connect.execute(f"CREATE TABLE {chat_id}(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT)")
-    def select_all_users(self):
-        all = self.connect.execute("SELECT * FROM `users`")
-        return all.fetchall()
-    def insert_name(self, name):
-        self.connect.execute("INSERT FROM `users` VALUES name=(?)", (name))
+def select_all_users():
+    all = connect.execute("SELECT * FROM `users`")
+    return all.fetchall()
+def insert_name(name):
+    connect.execute("INSERT FROM `users` VALUES name=(?)", (name))
 
 
 
